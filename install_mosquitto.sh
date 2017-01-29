26843d4e505b19249dc58ea77c6006a0181c5896
Fredrik Lindqvist <Landrash@gmail.com>
2017-01-29 16:20:26 +0100

    Inital commit

---
+++ install_mosquitto.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Install by running:
+# wget -Nnv https://raw.githubusercontent.com/dale3h/homeassistant-config/master/wizard/mosquitto-wizard.sh && sudo bash mosquitto-wizard.sh
+
+echo "Mosquitto Installer for Hassbian"
+echo "Copyright(c) 2016 Dale Higgs <https://gitter.im/dale3h>"
+echo
+
+echo "Adding mosquitto user"
+adduser subversion --system --group
+
+
+echo "Creating pid file"
+touch /var/run/mosquitto.pid
+chown mosquitto:mosquitto /var/run/mosquitto.pid
+
+echo "Creating data directory"
+mkdir -p /var/lib/mosquitto
+chown mosquitto:mosquitto /var/lib/mosquitto
+
+echo "Installing repository key"
+cd /srv/hass/src
+wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
+apt-key add mosquitto-repo.gpg.key
+
+echo "Adding repository"
+cd /etc/apt/sources.list.d
+wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
+
+echo "Installing mosquitto"
+apt-get update
+apt-cache search mosquitto
+apt-get install -y mosquitto mosquitto-clients
+
+echo "Writing default configuration"
+cd /etc/mosquitto
+mv mosquitto.conf mosquitto.conf.backup
+cp /home/pi/scripts/files/mosquitto.conf /etc/mosquitto/mosquitto.conf
+chown mosquitto:mosquitto mosquitto.conf
+
+echo "Initializing password file"
+touch passwd
+chown mosquitto:mosquitto passwd
+chmod 0600 passwd
+
+echo
+echo "Please take a moment to setup your first MQTT user"
+echo
+
+echo -n "Username: "
+read mqtt_username
+if [ ! "$mqtt_username" ]; then
+  mqtt_username=pi
+fi
+
+echo -n "Password: "
+read -s mqtt_password
+echo
+if [ ! "$mqtt_password" ]; then
+  mqtt_password=raspberry
+fi
+
+echo "Creating password entry for user $mqtt_username"
+mosquitto_passwd -b passwd "$mqtt_username" "$mqtt_password"
+
+echo "Restarting Mosquitto service"
+systemctl restart mosquitto.service
+
+ip_address=$(ifconfig | awk -F':' '/inet addr/&&!/127.0.0.1/{split($2,_," ");print _[1]}')
+
+echo
+echo "Installation done!"
+echo
+echo "Your MQTT broker is running at $ip_address:1883"
+echo
+echo "If you have issues with this script, please contact @Landrash on gitter.im"
+echo "Original script by @dale3h on gitter.im"
+echo
---
+++ install_openzwave.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Installed with the HASSbian image.
+
+echo 
+echo "Open Z-Wave Installer for Hassbian"
+echo "Copyright(c) 2016 Dale Higgs <https://gitter.im/dale3h>"
+echo 
+
+echo "Running apt-get preparation"
+apt-get update
+apt-get upgrade -y
+apt-get install -y git make python3-dev libudev-dev python3-sphinx python3-setuptools libgnutlsxx28 libgnutls28-dev libssl-dev
+
+echo "Changing to homeassistant user"
+sudo -u homeassistant -H /bin/bash <<EOF
+
+echo "Activating virtualenv"
+source /srv/homeassistant/bin/activate
+
+echo "Installing latest version of cython"
+pip3 install --upgrade cython==0.24.1
+
+echo "Creating source directory"
+mkdir -p /srv/homeassistant/src
+chown -R homeassistant:homeassistant /srv/homeassistant/src
+
+echo "Cloning python-openzwave"
+cd /srv/homeassistant/src
+git clone  https://github.com/OpenZWave/python-openzwave.git
+
+echo "Building python-openzwave"
+chown homeassistant:homeassistant python-openzwave
+cd python-openzwave
+git checkout python3
+make build
+make install
+
+echo "Deactivating virtualenv"
+deactivate
+EOF
+
+echo "Creating libmicrohttpd directory"
+cd /srv/homeassistant/src
+mkdir libmicrohttpd
+chown homeassistant:homeassistant libmicrohttpd
+cd /srv/homeassistant/src/libmicrohttpd
+
+echo "Downloading libmicrohttpd-0.9.19"
+wget ftp://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-0.9.19.tar.gz
+chown homeassistant:homeassistant libmicrohttpd-0.9.19.tar.gz
+tar zxvf libmicrohttpd-0.9.19.tar.gz
+chown homeassistant:homeassistant libmicrohttpd-0.9.19
+
+echo "Building libmicrohttpd-0.9.19"
+cd libmicrohttpd-0.9.19
+./configure
+make
+make install
+
+echo "Cloning open-zwave-control-panel"
+cd /srv/homeassistant/src
+git clone https://github.com/OpenZWave/open-zwave-control-panel.git
+chown -R homeassistant:homeassistant open-zwave-control-panel
+
+echo "Building open-zwave-control-panel"
+cd open-zwave-control-panel
+rm Makefile
+wget https://raw.githubusercontent.com/home-assistant/fabric-home-assistant/master/Makefile
+chown homeassistant:homeassistant Makefile
+make
+
+echo "Linking ozwcp config directory"
+ln -sd /srv/homeassistant/lib/python3.*/site-packages/libopenzwave-0.*-linux*.egg/config
+chown -R homeassistant:homeassistant /srv/homeassistant/src
+
+echo "Linking Home Assistant OpenZWave config directory"
+cd /home/homeassistant/.homeassistant
+sudo -u homeassistant ln -sd /srv/homeassistant/lib/python3.*/site-packages/libopenzwave-0.3.1-*-linux*.egg/config
+chown -R homeassistant:homeassistant /home/homeassistant/.homeassistant
+
+echo
+echo "Installation done!"
+echo
+echo "Blame @Landrash on gitter.im if this script breaks"
+echo "Original script by @Dal3h on gitter"
+echo 
