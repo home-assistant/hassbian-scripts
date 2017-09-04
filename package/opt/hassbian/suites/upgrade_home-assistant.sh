b58c1308d09c24669236d019115cba6b1db690e7
Ludeeus <ludeeus@users.noreply.github.com>
2017-09-04 20:23:52 +0200

    Added support for upgrade scripts (#42)
    * Added upgrade to MD
    
    * Initial upgrade script for homeassistant
    
    * Update and rename upgrade_homeassistant.sh to upgrade_home-assistant.sh
    
    * Update upgrade_home-assistant.sh
    
    * Initial support for upgrade
    
    * Update hassbian-config
    
    * Create upgrade_hassbian-script.sh
    
    * Fix minor spelling misstake
    
    * Spelling and cleanup of sentence

---
+++ package/opt/hassbian/suites/upgrade_home-assistant.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+function home-assistant-show-short-info {
+    echo "Home Assistant upgrade script for Hassbian"
+}
+
+function home-assistant-show-long-info {
+    echo "Upgrade the base Home Assistant package onto this system."
+}
+
+function home-assistant-show-copyright-info {
+    echo "Original concept by Landrash <https://github.com/Landrash>"
+    echo "Modyfied by Ludeeus <https://github.im/Ludeeus>"
+}
+
+function home-assistant-upgrade-package {
+home-assistant-show-short-info
+home-assistant-show-copyright-info
+
+
+echo "Stopping Home Assistant"
+systemctl stop home-assistant@homeassistant.service
+
+echo "Changing to the homeassistant user"
+sudo -u homeassistant -H /bin/bash << EOF
+
+echo "Creating Home Assistant venv"
+python3 -m venv /srv/homeassistant
+
+echo "Changing to Home Assistant venv"
+source /srv/homeassistant/bin/activate
+
+echo "Installing latest version of Home Assistant"
+pip3 install --upgrade homeassistant
+
+echo "Deactivating virtualenv"
+deactivate
+EOF
+
+echo "Restarting Home Assistant"
+systemctl start home-assistant@homeassistant.service
+
+echo
+echo "Uppgrade is now done."
+echo "Note tha it may take some time to start up after an upgrade."
+echo
+echo
+echo "If you have issues with this script, please say something in the #Hassbian channel on Discord."
+echo
+return 0
+}
+
+# Make this script function as it always has if run standalone, rather than issue a warning and do nothing.
+[[ $0 == "$BASH_SOURCE" ]] && home-assistant-upgrade-package
