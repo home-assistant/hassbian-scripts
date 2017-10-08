20408f8243ee39b2201c18d087da1688c18591e0
Fredrik Lindqvist <landrash@gmail.com>
2017-10-09 00:15:31 +0200

    Attempt to fix Tradfri again

--- package/opt/hassbian/suites/install_tradfri.sh
+++ package/opt/hassbian/suites/install_tradfri.sh
@@ -28,7 +28,6 @@ apt-get install -y libtool autoconf
 echo "Changing to homeassistant user"
 sudo -u homeassistant -H /bin/bash <<EOF
 
-
 echo "Creating source directory"
 mkdir -p /srv/homeassistant/src
 chown -R homeassistant:homeassistant /srv/homeassistant/src
@@ -41,21 +40,33 @@ cp configure.in configure.ac
 autoreconf
 ./configure --with-ecc --without-debug
 cd cython
+
+echo "Changing to Home Assistant venv"
+source /srv/homeassistant/bin/activate
+
 echo "Installingmodified tinydtls library"
 python3 setup.py install
+
+echo "Deactivating virtualenv"
+deactivate
+
 
 echo "Cloning modified lib-coap library"
 cd /srv/homeassistant/src
 git clone https://github.com/chrysn/aiocoap
 cd aiocoap
 git reset --hard 3286f48f0b949901c8b5c04c0719dc54ab63d431
+
+echo "Changing to Home Assistant venv"
+source /srv/homeassistant/bin/activate
+
 echo "Installing modified lib-coap library"
 python3 -m pip install .
-EOF
 
-echo "Installing lib-coap library"
-cd /srv/homeassistant/src/libcoap
-sudo make install
+echo "Deactivating virtualenv"
+deactivate
+
+EOF
 
 echo
 echo "Installation done."
