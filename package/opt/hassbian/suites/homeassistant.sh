a22e8792def0a38a4487baf7f02ad66900445eb6
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-02-23 23:02:27 +0100

    Fixed typo (#123)

--- package/opt/hassbian/suites/homeassistant.sh
+++ package/opt/hassbian/suites/homeassistant.sh
@@ -111,7 +111,7 @@ if [ ! -z "${validation}" ]; then
   echo
 else
   echo
-  echo -e "\\e[31mInstallation failed..."
+  echo -e "\\e[31mUpgrade failed..."
   echo -e "\\e[31mAborting..."
   echo -e "\\e[0mIf you have issues with this script, please say something in the #devs_hassbian channel on Discord."
   echo
