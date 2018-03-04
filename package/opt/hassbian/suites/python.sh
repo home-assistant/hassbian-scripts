4ee73235f7fcfe45d765610f3c7d7fa5bb2b3c8e
Joakim Sørensen <joasoe@gmail.com>
2018-03-04 16:07:05 +0100

    Fix for stickler-ci

--- package/opt/hassbian/suites/python.sh
+++ package/opt/hassbian/suites/python.sh
@@ -22,13 +22,14 @@ apt-get -y update
 apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 apt-get install libtcmalloc-minimal4
 export LD_PRELOAD="/usr/lib/libtcmalloc_minimal.so.4"
-cd /tmp
+cd /tmp || return 1
 wget https://www.python.org/ftp/python/"$PYTHONVERSION"/Python-"$PYTHONVERSION".tar.xz
 tar xf Python-"$PYTHONVERSION".tar.xz
-cd Python-"$PYTHONVERSION"
+cd Python-"$PYTHONVERSION" || return 1
 ./configure --enable-optimizations
 make
 apt -y autoremove
+cd  || return 1
 rm -r /tmp/Python-"$PYTHONVERSION"
 rm /tmp/Python-"$PYTHONVERSION".tar.xz
 echo "Done"
