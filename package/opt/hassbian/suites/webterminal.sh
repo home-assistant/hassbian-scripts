f43eb3007e8ca2b234fd9bb550810befd27e762b
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-03-13 12:30:48 +0100

    Webterminal: Added SSL support with existing LE certificates. (#130)
    * Added SSL support with existing LE certificates.

--- package/opt/hassbian/suites/webterminal.sh
+++ package/opt/hassbian/suites/webterminal.sh
@@ -12,17 +12,56 @@ function webterminal-show-copyright-info {
 }
 
 function webterminal-install-package {
+if [ "$ACCEPT" == "true" ]; then # True if `-y` flag is used.
+  if [ -d "/etc/letsencrypt/live" ] || [ -d "/home/homeassistant/dehydrated/certs" ]; then
+    SSL="Y"
+  else
+    SSL="N"
+  fi
+else
+  echo ""
+  echo -n "Do you use SSL (https) with Home Assistant? [N/y] : "
+  read -r SSL
+  if [ ! "$SSL" ]; then
+      SSL="N"
+  fi
+fi
+
 echo "Installing packages."
 sudo apt-get install -y openssl shellinabox
-
 echo "Changing config."
-sudo sed -i 's/--no-beep/--no-beep --disable-ssl/g' /etc/default/shellinabox
+if [ "$SSL" == "y" ] || [ "$SSL" == "Y" ]; then
+  echo "No need to change default configuration, skipping this step..."
+  echo "Checking cert directory..."
+  if [ -d "/etc/letsencrypt/live" ]; then
+    CERTDIR="/etc/letsencrypt/live/"
+  elif [ -d "/home/homeassistant/dehydrated/certs" ]; then
+    CERTDIR="/home/homeassistant/dehydrated/certs/"
+  else
+    CERTDIR=""
+  fi
+  echo "Merging files and adding to correct dir..."
+  DOMAIN=$(ls "$CERTDIR")
+  cat "$CERTDIR$DOMAIN/fullchain.pem" "$CERTDIR$DOMAIN/privkey.pem" > /var/lib/shellinabox/certificate-"$DOMAIN".pem
+  chown shellinabox:shellinabox -R /var/lib/shellinabox/
+  echo "Adding crong job to copy certs..."
+  (crontab -l ; echo "5 1 1 * * bash /opt/hassbian/suites/files/webterminalsslhelper.sh >/dev/null 2>&1")| crontab -
+else
+  sed -i 's/--no-beep/--no-beep --disable-ssl/g' /etc/default/shellinabox
+fi
 
 echo "Reloading and starting the service."
-sudo service shellinabox reload
-sudo service shellinabox restart
+service shellinabox reload
+service shellinabox stop
+service shellinabox start
 
 ip_address=$(ifconfig | grep "inet.*broadcast" | grep -v 0.0.0.0 | awk '{print $2}')
+
+if [ "$SSL" == "y" ] || [ "$SSL" == "Y" ]; then
+  PROTOCOL="https"
+else
+  PROTOCOL="http"
+fi
 
 echo "Checking the installation..."
 validation=$(pgrep -f shellinaboxd)
@@ -30,7 +69,7 @@ if [ ! -z "${validation}" ]; then
   echo
   echo -e "\\e[32mInstallation done..\\e[0m"
   echo
-  echo "You can now access the web terminal here: http://$ip_address:4200"
+  echo "You can now access the web terminal here: $PROTOCOL://$ip_address:4200"
   echo "You can also add this to your Home-Assistant config in an 'panel_iframe'"
   echo
 else
