53d65eaacc325905937080c46705650f34e386b2
Joakim Sørensen <joasoe@gmail.com>
2019-02-05 21:56:21 +0100

    cloud9: Add npm install to c9 script to make sure all packages are installed. (#236)
    * Add npm install to c9 script to make sure all packages are installed.
    
    * typo correction
    
    * Typo correction
    
    * only use NPM install if sdk install fails.

--- package/opt/hassbian/suites/cloud9.sh
+++ package/opt/hassbian/suites/cloud9.sh
@@ -48,9 +48,23 @@ echo "Starting Cloud9 service..."
 systemctl start cloud9@homeassistant.service
 
 echo "Checking the installation..."
-ip_address=$(ifconfig | grep "inet.*broadcast" | grep -v 0.0.0.0 | awk '{print $2}')
+sleep 15
 validation=$(pgrep -f cloud9)
 if [ ! -z "${validation}" ]; then
+  echo "Using fallback installation."
+  echo "Installing npm"
+  apt install -y npm
+
+  cd /opt/c9sdk || exit 1
+  npm install
+
+  echo "Checking the installation..."
+  sleep 15
+fi
+
+validation=$(pgrep -f cloud9)
+if [ -z "${validation}" ]; then
+  ip_address=$(ifconfig | grep "inet.*broadcast" | grep -v 0.0.0.0 | awk '{print $2}')
   echo
   echo -e "\\e[32mInstallation done.\\e[0m"
   echo "Your Cloud9 IDE is now avaiable at http://$ip_address:8181"
@@ -77,6 +91,7 @@ printf "Starting Cloud9 service...\\n"
 systemctl start cloud9@homeassistant.service
 
 echo "Checking the installation..."
+sleep 15
 validation=$(pgrep -f cloud9)
 if [ ! -z "${validation}" ]; then
   echo
