0f0af48b757f52c6e58cdaa202f4451e68357cbf
ludeeus <joasoe@gmail.com>
2019-04-15 12:33:31 +0200

    more changes

--- package/opt/hassbian/suites/python.sh
+++ package/opt/hassbian/suites/python.sh
@@ -27,7 +27,7 @@ fi
 isinstalled=$(which python${PYTHONVERSION:0:3})
 if [ ! -z "${isinstalled}" ]; then
   currentversion$(python${PYTHONVERSION:0:3} -V)
-  if [[ "$currentversion" != "$PYTHONVERSION" ]]; then
+  if [[ "${currentversion:6}" != "$PYTHONVERSION" ]]; then
     echo "Upgrading to Python $PYTHONVERSION"
     apt-get -y update
     apt-get install -y build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
